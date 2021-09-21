73bf84640726b1f12ca44218c2a74fc925933705
Lucas Mace <lucasmace4130@gmail.com>
2021-09-20 23:32:03 -0500

    Three

--- server.js
+++ server.js
@@ -16,9 +16,5 @@ app.get('/greeting', function (req, res, next) {
     res.json("Satan");
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, './client/public/index.html'));
-});
-
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`\n\n ❤️ Server listening on port: ${port} ❤️ \n\n`));
