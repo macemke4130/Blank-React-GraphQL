f22d2056fbfe7af4bff35da21e0ff0da188b693c
Lucas Mace <lucasmace4130@gmail.com>
2021-09-20 23:26:14 -0500

    Two

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/greeting', function (req, res, next) {
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, './client/public/index.html'));
 });
 
 const port = process.env.PORT || 3001;
